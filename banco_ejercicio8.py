8b858126eac4ac26b5b53b006e6c1bc075e4b1fb
octagomez <57689320+octagomez@users.noreply.github.com>
2021-06-05 17:47:22 -0300

    Actualizo ejercicio 8

--- banco_ejercicio8.py
+++ banco_ejercicio8.py
@@ -1,35 +1,38 @@
+from datetime import datetime
+
 class Banco:
     #funcion Cargar clientes
     #funcion cargar cuentas
     #funcion crear cliente
     #funcion crear cuenta
-    
+    #funcionar cliente en array
+    #funcionar cuenta en array
+    #funcionar empleado en array
+
+    #funcion
+
     
     def __init__(self):
         self.clientes=[]    #objeto de la clase cliente
         self.cuentas=[]      #objeto de la clase cliente
+        self.empleados=[]      #objeto de la clase cliente
 
     def operar(self):               
-        self.cliente1.depositar(100)
-        self.cliente2.depositar(150)
-        self.cliente3.depositar(200)
-        self.cliente3.extraer(150)
+
 
     def depositos_totales(self): #funcion que sume todos los montos de las cuentas x clientes
         total=self.cliente1.retornar_monto()+self.cliente2.retornar_monto()+self.cliente3.retornar_monto()
-        print("El total de dinero del banco es:",total)
-        self.cliente1.imprimir()
-        self.cliente2.imprimir()
-        self.cliente3.imprimir()
-
+      
 class Cuenta:
     
     def __init__ (self,Cliente, monto):
         self.cliente=Cliente
         self.monto=monto
+        self.nro_cuenta = "001" + datetime.now()
         
     def imprimir(self):
         print("Titular: ", self.cliente.nombre)
+        print("Nro Cuenta: ", self.nro_cuenta)
         print("Monto: ", self.monto)
         
     def depositar(self,monto):   #metodo para sumar un deposito al monto del cliente
@@ -40,14 +43,9 @@ class Cuenta:
 
     def retornar_monto(self):     #metodo para mostar el saldo de cada cliente, me sirve para luego sumar todos los montos y sacar el dinero que tiene el banco
         return self.monto
-
-    def imprimir(self):         #metodo
-        print(self.nombre," Tiene depositado la suma de: ",self.monto)
            
 class CajaDeAhorro(Cuenta):   #clase CDA hereda la clase cuenta
-    
-    def __init__ (self, titular, monto) : 
-        super().__init__(titular, monto)   #lo hereda de la clase cuenta, lo llamo
+     #lo hereda de la clase cuenta, lo llamo
         
     def imprimir(self):
         print("Cuenta Caja de Ahorro")
@@ -55,10 +53,12 @@ class CajaDeAhorro(Cuenta):   #clase CDA hereda la clase cuenta
         
 class PlazoFijo(Cuenta):
     
-    def __init__(self, titular, monto, plazo, interes):   #plazo fijo ademas de los atributos titular y monto tiene un plazo e interes de ese plazo
-        super().__init__(titular,monto)
+    def __init__(self, Cliente, monto, plazo, interes):   #plazo fijo ademas de los atributos titular y monto tiene un plazo e interes de ese plazo
+        super().__init__(Cliente,monto)
         self.plazo=plazo   #inicializo el atributo plazo y le copio lo que llega en el parámetro
-        self.interes=interes    #inicializo el atributo interes y le copio lo que llega en el parámetro
+        self.monto = monto    #inicializo el atributo interes y le copio lo que llega en el parámetro
+        self.plazo = plazo
+        self.interes = interes
         
     def imprimir(self):
         print("Cuenta de Plazo Fijo")
@@ -66,20 +66,38 @@ class PlazoFijo(Cuenta):
         print("Plazo en días: ", self.plazo)
         print("Intereses: ", self.interes)
         self.ganancia()
-        
+          
     def ganancia(self):
         ganancia=self.monto*self.interes/100
-        print("Monto de Intereses: ", ganancia)
+        return ganancia
 
 # creamos la clase Cliente
 class Cliente:
-    def __init__(self,nombre,telefono,mail):
-        self.nombre = nombre
-        self.telefono = telefono
-        self.mail = mail
+
+    def __init__(self):
+        self.nombre = ""
+        self.telefono = 0
+        self.mail = ""
     
     def cargar_cliente(self):
         self.nombre = input("Nombre: " )
         self.telefono = int(input("Telefono: "))
         self.mail = input("Nombre: " )
-    
+
+#creo un cliente
+cliente = Cliente()
+cliente.cargar_cliente()
+
+#creo un una caja de ahorro
+#solicito monto
+monto = float(input("Ingrese inicial de caja de ahorro: "))
+#instancio el objeto caja de ahorro
+ca_cliente = CajaDeAhorro(cliente, monto)
+
+#crear un plazo fijo
+#solicito los datos del plazo fijo
+interes = float(input("Ingrese interes: "))
+plazo = float(input("Ingrese plazo: "))
+monto = float(input("Ingrese monto: "))
+
+pf_ciente = PlazoFijo(cliente,monto,plazo,interes)
